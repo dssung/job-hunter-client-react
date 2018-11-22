05ed0d284e0d0c1f24fd7699d5bc44f2fb15f33b
Daniel Sung <d2sung@ucsd.edu>
2018-11-22 01:59:37 -0800

    Google maps api fix

--- src/indeedJobs/components/LocationSearchInput.js
+++ src/indeedJobs/components/LocationSearchInput.js
@@ -10,7 +10,10 @@ class LocationSearchInput extends React.Component{
        <PlacesAutocomplete
           value = {this.props.value}
           onChange = {this.props.onChange}
-          
+          searchOptions = {{
+            types: ['(cities)'],
+            componentRestrictions: {country: 'us'}
+          }}
         >
           {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
             <div>
